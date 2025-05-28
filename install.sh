aaf6aad0ee72130c344938f27df5685d3bd815b9
clientruncontext <jifdosggfdoijg@gmail.com>
2025-05-28 23:15:55 +0200

    update install script
    made it create the dirs

--- install.sh
+++ install.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+mkdir -p $HOME/Pictures
+mkdir -p $HOME/Pictures/Screenshots
+mkdir -p $HOME/Downloads
+mkdir -p $HOME/Desktop
+mkdir -p $HOME/Documents
+mkdir -p $HOME/Projects
+
 sudo nixos-rebuild switch --flake .#default --impure
 sudo nixos-rebuild switch --flake .#fix_nixpkgs --impure
